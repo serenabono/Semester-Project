f0b5a7976f3a62e47a249f84718a9c4317191ae3
Serena Bono <sebono@student.ethz.ch>
2021-06-03 10:22:51 +0200

    Update genSets.py

--- generate synthetic images/genSets.py
+++ generate synthetic images/genSets.py
@@ -1,5 +1,5 @@
 import random, os
-"""
+
 #REAL IMAGES
 path = "./realdata/"
 #create Train Dataset
@@ -18,7 +18,7 @@ for i in range(150):
     ])
     os.rename(path+random_filename, "./testA/"+random_filename)
 print("finished with TestA")
-"""
+
 label_prev1 = open('labels_seq1.txt', 'r')
 label_prev2 = open('labels_seq2.txt', 'r')
 #SYNTHETIC IMAGES
@@ -28,37 +28,45 @@ labeltxt = open('dataset_train.txt', 'w')
 lines1=label_prev1.readlines()
 lines2=label_prev2.readlines()
 
+#imput sequence file starting number
+n=2304
 for i in range(0,3000):
     random_filename = random.choice([
         x for x in os.listdir(path)
         if os.path.isfile(os.path.join(path, x))
     ])
     name=int(random_filename.split(".")[0])
-    if(name> 2303):
-        name-=2303
+    if(name> n):
+        name-=n
         labeltxt.write(lines1[name])
     else:
         labeltxt.write(lines2[name])
-    #os.rename(path+random_filename, "/itet-stor/sebono/net_scratch/datasets/fieldboundary/images/trainB-wrobot/"+random_filename)
+    #move file in correct folder 
+    os.rename(path+random_filename, "/itet-stor/sebono/net_scratch/datasets/fieldboundary/images/trainB-wrobot/"+random_filename)
 
 print("finished with TrainB")
 labeltxt.close()
-
 labeltxt = open('dataset_test.txt', 'w')
+
 #create Test Dataset
+
+#imput sequence file starting number
+n=2304
 for i in range(0,300):
     random_filename = random.choice([
         x for x in os.listdir(path)
         if os.path.isfile(os.path.join(path, x))
     ])
     name=int(random_filename.split(".")[0])
-    if(name> 2303):
-        name-=2303
+    
+    if(name> n):
+        name-=n
         labeltxt.write(lines1[name])
     else:
         labeltxt.write(lines2[name])
-
-    #os.rename(path+random_filename, "/itet-stor/sebono/net_scratch/datasets/fieldboundary/images/testB-wrobot/"+random_filename)
+    
+    #move file in correct folder
+    os.rename(path+random_filename, "/itet-stor/sebono/net_scratch/datasets/fieldboundary/images/testB-wrobot/"+random_filename)
 
 print("finished with TestB")
 labeltxt.close()
